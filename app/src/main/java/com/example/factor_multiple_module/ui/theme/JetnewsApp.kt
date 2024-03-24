1b5113d98c3fcebb39dd0e34731009b98ee73f4b
c004245 <c004245@naver.com>
2024-03-24 17:52:15 +0900

    chore: common theme, reconnect navigation state

---
+++ app/src/main/java/com/example/factor_multiple_module/ui/theme/JetnewsApp.kt
@@ -0,0 +1,27 @@
+package com.example.factor_multiple_module.ui.theme
+
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun JetnewsApp(
+    widthSizeClass: WindowWidthSizeClass
+) {
+    FactormultiplemoduleTheme {
+        val navController = rememberNavController()
+        val navigationActions = remember(navController) {
+            JetnewsNavigationActions(navController = navController)
+        }
+
+        val coroutineScope = rememberCoroutineScope()
+
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+
+        val 
+
+    }
+}
